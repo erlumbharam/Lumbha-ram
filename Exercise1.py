55f56d7352958415e762ff1a11dcba038b05643d
erlumbharam <63182965+erlumbharam@users.noreply.github.com>
2020-04-05 13:07:01 +0530

    Ex1Task1.py

--- Exercise1.py
+++ Exercise1.py
@@ -1,9 +1,8 @@
 #Lumbha-ram
 #Computerscience6thsem
 #TASK_1
-#(Write_a_program_which_will_find_all_such_numbers_which_are_divisible_by_7_but are not a multiple
-of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a
-comma-separated sequence on a single line)
+#(Write_a_program_which_will_find_all_such_numbers_which_are_divisible_by_7_but_are_not_a_multipleof_5,_between_2000_and_3200_(both included)._The_numbers_obtained_should_be_printed_in_a
+comma_separated_sequence_on_a_single_line)
 
 l=[ ]
 for i in range(2000, 3201):
