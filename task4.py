89f485d63b6d6fb7a06f506bfe53ce6ccfdbdf6f
LUMBHA RAM <63182965+erlumbharam@users.noreply.github.com>
2020-04-06 15:22:10 +0530

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,7 @@
+# Lumbha-ram
+#EXERCISE_1_BY_PBN_SIR
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('Tuple : ',tuple)
